5a997f4c7f7ef127b8f2a39aabc574c6d2c22422
Johannes Endres <je@johannes-endres.de>
2022-07-23 17:17:46 +0200

    Remove debug code

--- src/management/management.js
+++ src/management/management.js
@@ -1,8 +1,6 @@
 import { Localize } from "../lib/localize.js";
 import { FormHandler } from "./formhandler.js";
 
-console.debug("Dialog added");
-
 Localize.addLocalizedLabels();
 
 const formHandler = new FormHandler(new URL(location.href).searchParams.get("accountId"));
