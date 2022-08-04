3c98190a0289a1ccd01e9f6d69e4e3d202b350f8
Johannes Endres <je@johannes-endres.de>
2022-08-04 14:36:22 +0000

    Resolve "Add browser. Types"

--- src/background.js
+++ src/background.js
@@ -1,11 +1,11 @@
 AccountUpdater.update_all();
 TBVersionWorkarounds.apply_all();
 
-messenger.cloudFile.onFileUpload.addListener(EventHandlers.onFileUpload);
-messenger.cloudFile.onFileUploadAbort.addListener(EventHandlers.onFileUploadAbort);
-messenger.cloudFile.onFileDeleted.addListener(EventHandlers.onFileDeleted);
-// messenger.cloudFile.onAccountAdded.addListener( /* Nothing to do, don't add a handler*/
-messenger.cloudFile.onAccountDeleted.addListener(EventHandlers.onAccountDeleted);
+browser.cloudFile.onFileUpload.addListener(EventHandlers.onFileUpload);
+browser.cloudFile.onFileUploadAbort.addListener(EventHandlers.onFileUploadAbort);
+browser.cloudFile.onFileDeleted.addListener(EventHandlers.onFileDeleted);
+// browser.cloudFile.onAccountAdded.addListener( /* Nothing to do, don't add a handler*/
+browser.cloudFile.onAccountDeleted.addListener(EventHandlers.onAccountDeleted);
 
 /* Make jshint happy */
 /* global AccountUpdater, EventHandlers, TBVersionWorkarounds */
