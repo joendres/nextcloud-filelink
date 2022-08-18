ae166ed121e57b50248aa571f875cb814af5b367
Johannes Endres <je@johannes-endres.de>
2022-08-18 18:55:08 +0200

    Tet newly defined method

--- test/tests/utils.test.js
+++ test/tests/utils.test.js
@@ -1,17 +1,32 @@
-import { Utils } from "../../src/background/utils.js";
+import { Utils } from "../../src/common/utils.js";
 
-describe("Utils.encodepath", () => {
-    it("should return the input sting if it contains allowed characters only", () => {
-        const test_string = "/ABC/DEF/GHI/JKL/MNO/PQR/STU/VWX/YZ_abcdefghijklmnopqrstuvwxyz-0123456789/../.";
-        expect(Utils.encodepath(test_string)).to.equal(test_string);
+describe("Utils", () => {
+    describe("encodepath", () => {
+        it("returns the input sting if it contains allowed characters only", () => {
+            const test_string = "/ABC/DEF/GHI/JKL/MNO/PQR/STU/VWX/YZ_abcdefghijklmnopqrstuvwxyz-0123456789/../.";
+            expect(Utils.encodepath(test_string)).to.equal(test_string);
+        });
+        it("reproduces empty path parts", () => {
+            expect(Utils.encodepath("///")).to.equal("///");
+        });
+        it("encodes common special characters", () => {
+            expect(Utils.encodepath("/;,?:@&=+$/")).to.equal("/%3B%2C%3F%3A%40%26%3D%2B%24/");
+        });
+        it("encodes some extra characters: !()*'", () => {
+            expect(Utils.encodepath("!()*'")).to.equal("%21%28%29%2a%27");
+        });
     });
-    it("should reproduce empty path parts", () => {
-        expect(Utils.encodepath("///")).to.equal("///");
-    });
-    it("should encode common special characters", () => {
-        expect(Utils.encodepath("/;,?:@&=+$/")).to.equal("/%3B%2C%3F%3A%40%26%3D%2B%24/");
-    });
-    it("should encode some extra characters: ~*'", () => {
-        expect(Utils.encodepath("!()~*'")).to.equal("%21%28%29%7e%2a%27");
+
+    describe("encodeRFC3986", () => {
+        it("returns the input sting if it contains allowed characters only", () => {
+            const test_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-0123456789...";
+            expect(Utils.encodeRFC3986(test_string)).to.equal(test_string);
+        });
+        it("encodes common special characters", () => {
+            expect(Utils.encodeRFC3986(";,?:@&=+$")).to.equal("%3B%2C%3F%3A%40%26%3D%2B%24");
+        });
+        it("encodes some extra characters: !()*'", () => {
+            expect(Utils.encodeRFC3986("!()*'")).to.equal("%21%28%29%2a%27");
+        });
     });
 });
