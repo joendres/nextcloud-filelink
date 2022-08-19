ad76224c6b306e6ed5d272b2d86da0d75c8071bb
Johannes Endres <je@johannes-endres.de>
2022-08-19 23:11:35 +0200

    Test UploadStatus

---
+++ test/tests/uploadstatus.test.js
@@ -0,0 +1,17 @@
+import { UploadStatus } from "../../src/common/uploadstatus.js";
+import { Statuses } from "../../src/common/statuses.js";
+const expect = chai.expect;
+
+describe("UploadStatus", () => {
+    describe("constructor", () => {
+        it("sets the expected values", () => {
+            const us = new UploadStatus("filename");
+            expect(us).to.eql({
+                filename: "filename",
+                status: Statuses.PREPARING,
+                progress: 0,
+                error: false,
+            });
+        });
+    });
+});
