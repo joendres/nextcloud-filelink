52a566b0a70efe78c4aa5dd7fe125a4591ff45f6
Johannes Endres <je@johannes-endres.de>
2022-08-20 10:26:02 +0200

    test folderfieldhandler

---
+++ test/tests/folderfieldhandler.test.js
@@ -0,0 +1,57 @@
+import { FolderFieldHandler } from "../../src/management/folderfieldhandler.js";
+const expect = chai.expect;
+
+describe("FolderFieldHandler", () => {
+    describe("preCloudUpdate", () => {
+        afterEach(sinon.restore);
+
+        it("adds a missing / at the start", () => {
+            const node = { value: "path" };
+            sinon.stub(document, "querySelector").returns(node);
+
+            FolderFieldHandler.preCloudUpdate();
+
+            expect(node.value).to.equal("/path");
+        });
+        it("removes a / at the end", () => {
+            const node = { value: "/path/" };
+            sinon.stub(document, "querySelector").returns(node);
+
+            FolderFieldHandler.preCloudUpdate();
+
+            expect(node.value).to.equal("/path");
+        });
+        it("removes a sequence of / at the end", () => {
+            const node = { value: "/path///" };
+            sinon.stub(document, "querySelector").returns(node);
+
+            FolderFieldHandler.preCloudUpdate();
+
+            expect(node.value).to.equal("/path");
+        });
+        it("replaces a sequence of / at the start with only one", () => {
+            const node = { value: "///path" };
+            sinon.stub(document, "querySelector").returns(node);
+
+            FolderFieldHandler.preCloudUpdate();
+
+            expect(node.value).to.equal("/path");
+        });
+        it("replaces a sequence of / at in the path with only one", () => {
+            const node = { value: "/path///to" };
+            sinon.stub(document, "querySelector").returns(node);
+
+            FolderFieldHandler.preCloudUpdate();
+
+            expect(node.value).to.equal("/path/to");
+        });
+        it("leaves dots untouched", () => {
+            const node = { value: "/path/../up/./same" };
+            sinon.stub(document, "querySelector").returns(node);
+
+            FolderFieldHandler.preCloudUpdate();
+
+            expect(node.value).to.equal("/path/../up/./same");
+        });
+    });
+});
