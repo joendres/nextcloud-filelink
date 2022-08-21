a57ea1e0a7c1d7c453cea07ae42d1c6997d60ef7
Johannes Endres <je@johannes-endres.de>
2022-08-21 21:55:37 +0200

    Describe MessageDispatcher

--- test/tests/messagedispatcher.test.js
+++ test/tests/messagedispatcher.test.js
@@ -1,21 +1,86 @@
 import { MessageDispatcher } from "../../src/background/messagedispatcher.js";
+import { Status } from "../../src/background/status.js";
 const expect = chai.expect;
 
-describe("MessageDispatcher", () => {
+describe.only("MessageDispatcher", () => {
+
     describe('installHandler', () => {
-        it("installs the handler");
-        it("doesn't install the handler if its already installed");
+        afterEach(sinon.restore);
+
+        beforeEach(() => {
+            browser.runtime.onConnect.addListener = sinon.fake();
+        });
+
+        it("installs the handler", () => {
+            browser.runtime.onConnect.hasListener = sinon.fake.returns(false);
+            MessageDispatcher.installHandler();
+            expect(browser.runtime.onConnect.addListener.called).to.be.true;
+            expect(browser.runtime.onConnect.addListener.lastCall.firstArg).to.equal(MessageDispatcher.connectHandler);
+        });
+        it("doesn't install the handler if its already installed", () => {
+            browser.runtime.onConnect.hasListener = sinon.fake.returns(true);
+            MessageDispatcher.installHandler();
+            expect(browser.runtime.onConnect.addListener.called).to.be.false;
+        });
     });
 
     describe('connectHandler', () => {
-        it("stores the port in the Status class");
-        it("installs a disconnect handler");
-        it("installs the dispatcher as handler");
+        let thePort = {
+            onDisconnect: {},
+            onMessage: {},
+        };
+
+        afterEach(sinon.restore);
+
+        beforeEach(() => {
+            thePort.onDisconnect.addListener = sinon.fake();
+            thePort.onMessage.addListener = sinon.fake();
+        });
+
+        it("stores the port in the Status class", () => {
+            delete Status.port;
+            MessageDispatcher.connectHandler(thePort);
+            expect(Status.port).to.deep.equal(thePort);
+        });
+        it("installs a disconnect handler", () => {
+            MessageDispatcher.connectHandler(thePort);
+            expect(thePort.onDisconnect.addListener.called).to.be.true;
+            expect(thePort.onDisconnect.addListener.lastCall.firstArg).to.equal(MessageDispatcher.closePort);
+        });
+        it("installs the dispatcher as handler", () => {
+            MessageDispatcher.connectHandler(thePort);
+            expect(thePort.onMessage.addListener.called).to.be.true;
+            expect(thePort.onMessage.addListener.lastCall.firstArg).to.equal(MessageDispatcher.dispatch);
+        });
+    });
+
+    describe('closePort', () => {
+        it("Sets Status.port to null", () => {
+            Status.port = "notnull";
+            MessageDispatcher.closePort();
+            expect(Status.port).to.be.null;
+        });
     });
 
     describe('dispatch', () => {
-        it("dispatches the update command");
-        it("dispatches the clear_complete command");
-        it("throws a ReferenceError on another command");
+        afterEach(sinon.restore);
+
+        it("dispatches the update command", () => {
+            sinon.stub(Status, "update");
+            MessageDispatcher.dispatch("update");
+            expect(Status.update.called).to.be.true;
+        });
+
+        it("dispatches the clearcomplete command", () => {
+            sinon.stub(Status, "clearcomplete");
+            MessageDispatcher.dispatch("clearcomplete");
+            expect(Status.clearcomplete.called).to.be.true;
+        });
+        it("throws a ReferenceError on add command", () => {
+            expect(() => MessageDispatcher.dispatch("add")).to.throw(ReferenceError, "No handler for add");
+        });
+        it("throws a ReferenceError on another command", () => {
+            expect(() => MessageDispatcher.dispatch("the_Spanish_inquisition")).to.throw(ReferenceError, "No handler for the_Spanish_inquisition");
+        });
     });
 });
