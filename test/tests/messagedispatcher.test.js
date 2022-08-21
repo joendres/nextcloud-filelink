6fd18a27ee21f9141449018021d2261ed252162e
Johannes Endres <je@johannes-endres.de>
2022-08-21 18:52:20 +0200

    describe the behavior

--- test/tests/messagedispatcher.test.js
+++ test/tests/messagedispatcher.test.js
@@ -2,5 +2,20 @@ import { MessageDispatcher } from "../../src/background/messagedispatcher.js";
 const expect = chai.expect;
 
 describe("MessageDispatcher", () => {
-    /** @todo Add tests */
+    describe('installHandler', () => {
+        it("installs the handler");
+        it("doesn't install the handler if its already installed");
+    });
+
+    describe('connectHandler', () => {
+        it("stores the port in the Status class");
+        it("installs a disconnect handler");
+        it("installs the dispatcher as handler");
+    });
+
+    describe('dispatch', () => {
+        it("dispatches the update command");
+        it("dispatches the clear_complete command");
+        it("throws a ReferenceError on another command");
+    });
 });
