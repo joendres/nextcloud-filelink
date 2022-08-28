723d7a3c2423920db738b4d5613eb00409ce073b
Johannes Endres <je@johannes-endres.de>
2022-08-28 10:25:42 +0200

    Test Popup

--- test/tests/popup.test.js
+++ test/tests/popup.test.js
@@ -2,8 +2,7 @@ import { Popup } from "../../src/management/popup/popup.js";
 import { Localize } from "../../src/common/localize.js";
 const expect = chai.expect;
 
-describe.only("Popup", () => {
-    /** @todo Add tests */
+describe("Popup", () => {
     describe('error', () => {
         beforeEach(() => {
             sinon.stub(Popup, "openPopup");
@@ -100,51 +99,101 @@ describe.only("Popup", () => {
         });
     });
     describe('openPopup', () => {
-        it("fetches the template");
-        it("does nothing if the template is missing");
-        it("clones the template");
-        it("adds the message");
-        it("makes the popup visible");
-        it("adds the click handler to the button");
-        it("adds the popup to the msg_container");
+        beforeEach(() => {
+            const msg_container = document.createElement("div");
+            const popup = document.createElement("div");
+            let element = document.createElement("div");
+            element.classList.add("popup_message");
+            popup.appendChild(element);
+            element = document.createElement("button");
+            element.classList.add("msg_bar_closebtn");
+            popup.appendChild(element);
+            sinon.stub(document, "querySelector").
+                withArgs("#msg_container").returns(msg_container).
+                withArgs(sinon.match(/^#\w+_popup$/)).returns(popup);
+        });
+        afterEach(sinon.restore);
+        it("fetches the template", () => {
+            Popup.openPopup("error", "error");
+            expect(document.querySelector.withArgs("#error_popup").calledOnce).to.be.true;
+            expect(document.querySelector.lastCall.firstArg).to.equal("#error_popup");
+        });
+        it("throws a TypeError if the template is missing", () => {
+            sinon.restore();
+            expect(() => Popup.openPopup("missing", "missing")).to.throw(TypeError);
+        });
+        it("adds the popup to the msg_container", () => {
+            const spy = sinon.spy(document.querySelector("#msg_container"), "appendChild");
+
+            Popup.openPopup("error", "error");
+            expect(spy.calledOnce).to.be.true;
+            expect(spy.lastCall.firstArg).to.be.a("HTMLDivElement");
+        });
+        it("returns the popup", () => {
+            const p = Popup.openPopup("error", "error");
+            expect(p).to.be.a("HTMLDivElement");
+        });
+        it("the popup contains the message", () => {
+            const p = Popup.openPopup("error", "The Spanish Inquisition");
+            expect(p.innerHTML).to.contain("The Spanish Inquisition");
+        });
+        it("the popup is not hidden", () => {
+            const p = Popup.openPopup("error", "error");
+            expect(p.hidden).to.be.false;
+        });
+        it("the click handler is attached to the button", () => {
+            /** @type {HTMLDivElement} */
+            const p = Popup.openPopup("error", "error");
+            for (let i = 0; i < p.children.length; i++) {
+                if ("BUTTON" === p.children[i].tagName) {
+                    expect(p.children[i].onclick).to.equal(Popup.close);
+                }
+            }
+
+        });
     });
     describe('close', () => {
-        it("removes the popup that contains the close button");
-    });
-    describe('clear', () => {
-        beforeEach(() => {
-            const msg_container = {
-                canary: "alive",
-                open_popups: 1,
-            };
-            sinon.stub(document, "querySelector").returns(msg_container);
-            msg_container.firstChild = {
-                remove: sinon.fake(() => {
-                    if (msg_container.firstChild.remove.callCount === msg_container.open_popups) {
-                        delete msg_container.firstChild;
-                    }
-                })
-            };
-        });
-        afterEach(() => {
+        it("removes the popup that contains the close button", () => {
+            const popup = document.createElement("div");
+            const button = document.createElement("button");
+            popup.appendChild(button);
+            sinon.stub(popup, "remove");
+
+            button.onclick = Popup.close;
+            button.click();
+            expect(popup.remove.calledOnce).to.be.true;
+
             sinon.restore();
         });
+    });
+    describe('clear', () => {
+        var spy;
+        beforeEach(() => {
+            const msg_container = document.createElement("div");
+            sinon.stub(document, "querySelector").returns(msg_container);
+            spy = sinon.spy(Element.prototype, "remove");
+        });
+        afterEach(sinon.restore);
         it("removes an open popup", () => {
+            document.querySelector().appendChild(document.createElement("div"));
             Popup.clear();
-            expect(document.querySelector()).not.to.have.property("firstChild");
+            expect(spy.callCount).to.equal(1);
+            expect(document.querySelector().childElementCount).to.equal(0);
             expect(Popup.empty()).to.be.true;
         });
         it("removes three popups", () => {
-            document.querySelector().open_popups = 3;
+            const mc = document.querySelector();
+            mc.appendChild(document.createElement("div"));
+            mc.appendChild(document.createElement("div"));
+            mc.appendChild(document.createElement("div"));
             Popup.clear();
-            expect(document.querySelector()).not.to.have.property("firstChild");
+            expect(spy.callCount).to.equal(3);
+            expect(document.querySelector().childElementCount).to.equal(0);
             expect(Popup.empty()).to.be.true;
         });
         it("does nothing if there is no open popup", () => {
             const mc = document.querySelector();
-            delete mc.firstChild;
-            let mc_before = {};
-            Object.assign(mc_before, mc);
+            const mc_before = mc.cloneNode(true);
             Popup.clear();
             expect(mc).to.deep.equal(mc_before);
         });
