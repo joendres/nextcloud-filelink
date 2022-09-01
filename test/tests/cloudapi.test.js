ffe1fcf29a9fe648455afe007506bae3bdda2b2f
Johannes Endres <je@johannes-endres.de>
2022-09-01 21:59:26 +0200

    describe tests

--- test/tests/cloudapi.test.js
+++ test/tests/cloudapi.test.js
@@ -3,13 +3,51 @@ const expect = chai.expect;
 
 describe("CloudAPI", () => {
     /** @todo Add tests */
-    describe('getQuota', () => { });
-    describe('getCapabilitiesAndVersion', () => { });
-    describe('getUserId', () => { });
-    describe('getAppPassword', () => { });
-    describe('validateDownloadPassword', () => { });
-    describe('getGeneratedPassword', () => { });
-    describe('getSharesForFile', () => { });
-    describe('getNewShare', () => { });
     describe('doApiCall', () => { });
+    describe('getAppPassword', () => {
+        beforeEach(() => {
+            sinon.stub(CloudAPI, "doApiCall");
+        });
+        afterEach(sinon.restore);
+        it("is a static method");
+        it("is an async function");
+        it("returns null if the API call fails");
+        it("returns null it the API call doesn't return a password");
+        it("returns the password from the API call");
+    });
+    describe('getCapabilitiesAndVersion', () => {
+        it("is a static method");
+        it("is an async function");
+        it("returns null if the API call fails");
+    });
+    describe('getGeneratedPassword', () => {
+        it("is a static method");
+        it("is an async function");
+        it("returns null if the API call fails");
+    });
+    describe('getNewShare', () => {
+        it("is a static method");
+        it("is an async function");
+        it("returns null if the API call fails");
+    });
+    describe('getQuota', () => {
+        it("is a static method");
+        it("is an async function");
+        it("returns null if the API call fails");
+    });
+    describe('getSharesForFile', () => {
+        it("is a static method");
+        it("is an async function");
+        it("returns null if the API call fails");
+    });
+    describe('getUserId', () => {
+        it("is a static method");
+        it("is an async function");
+        it("returns null if the API call fails");
+    });
+    describe('validateDownloadPassword', () => {
+        it("is a static method");
+        it("is an async function");
+        it("returns null if the API call fails");
+    });
 });
