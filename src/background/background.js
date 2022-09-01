5fa3a7da504496830fd27421a9c2ca98827baef8
Johannes Endres <je@johannes-endres.de>
2022-09-01 21:47:57 +0200

    Test background.js more intelligently

--- src/background/background.js
+++ src/background/background.js
@@ -3,17 +3,13 @@ import { EventHandlers } from "./eventhandlers.js";
 import { MessageDispatcher } from "./messagedispatcher.js";
 import { TBVersionWorkarounds } from "./tbversionworkarounds.js";
 
-main();
+AccountUpdater.update_all();
+TBVersionWorkarounds.apply_all();
 
-export function main() {
-  AccountUpdater.update_all();
-  TBVersionWorkarounds.apply_all();
+browser.cloudFile.onFileUpload.addListener(EventHandlers.onFileUpload);
+browser.cloudFile.onFileUploadAbort.addListener(EventHandlers.onFileUploadAbort);
+browser.cloudFile.onFileDeleted.addListener(EventHandlers.onFileDeleted);
+browser.cloudFile.onAccountAdded.addListener(EventHandlers.onAccountAdded);
+browser.cloudFile.onAccountDeleted.addListener(EventHandlers.onAccountDeleted);
 
-  browser.cloudFile.onFileUpload.addListener(EventHandlers.onFileUpload);
-  browser.cloudFile.onFileUploadAbort.addListener(EventHandlers.onFileUploadAbort);
-  browser.cloudFile.onFileDeleted.addListener(EventHandlers.onFileDeleted);
-  browser.cloudFile.onAccountAdded.addListener(EventHandlers.onAccountAdded);
-  browser.cloudFile.onAccountDeleted.addListener(EventHandlers.onAccountDeleted);
-
-  MessageDispatcher.installHandler();
-}
+MessageDispatcher.installHandler();
