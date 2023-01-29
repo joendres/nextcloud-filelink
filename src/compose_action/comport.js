88f65ea297e7d845f336b2fe478f08c0b3fc1e3a
Johannes Endres <je@johannes-endres.de>
2023-01-29 13:03:38 +0100

    Improve documentation

--- src/compose_action/comport.js
+++ src/compose_action/comport.js
@@ -2,9 +2,10 @@ import { StatusDisplay } from "./statusdisplay.js";
 
 export class ComPort {
     static setup() {
-        /** Establish messaging with background worker
-         * @type {browser.runtime.Port} */
+        // Establish messaging with background worker
+        /** @type {browser.runtime.Port} */
         ComPort.port = browser.runtime.connect();
+        // Set the handler for the messages sent in Status.update()
         ComPort.port.onMessage.addListener(StatusDisplay.update);
         ComPort.port.postMessage("update");
 
@@ -15,6 +16,6 @@ export class ComPort {
     }
 
     static clearButtonHandler() {
-        ComPort.port.postMessage('clearcomplete');
+        ComPort.port.postMessage("clearcomplete");
     }
 }
