bc964ff6d6127a6d5b90bcbfdab63b72a310139a
Johannes Endres <je@johannes-endres.de>
2023-08-29 14:47:14 +0200

    Close #476

--- src/lib/cloudconnection.js
+++ src/lib/cloudconnection.js
@@ -79,19 +79,7 @@ class CloudConnection {
             let url = this._cleanUrl(await this._getShareLink(fileName, uploadId));
             if (url) {
                 // Add additional information introduced in TB 98
-                let templateInfo = {
-                    download_password_protected: this.useDlPassword,
-                };
-                if (this.useExpiry) {
-                    templateInfo.download_expiry_date = {
-                        timestamp: Date.now() + this.expiryDays * 24 * 60 * 60 * 1000,
-                        format: {
-                            day: "2-digit",
-                            month: "2-digit",
-                            year: "numeric",
-                        },
-                    };
-                }
+                let templateInfo = this._fillTemplate();
                 if (upload_status.status !== 'generatedpassword') {
                     Status.remove(uploadId);
                 }
@@ -105,6 +93,28 @@ class CloudConnection {
 
         upload_status.fail();
         throw new Error("Upload failed.");
+    }
+
+    /**
+     * Set information used to fill the text template in the message, currently
+     * only the fields download_password_protected and download_expiry_date
+     * @returns {CloudFileTemplateInfo} The relevant information for the current upload
+     */
+    _fillTemplate() {
+        let templateInfo = {
+            download_password_protected: this.useDlPassword,
+        };
+        if (this.useExpiry) {
+            templateInfo.download_expiry_date = {
+                timestamp: Date.now() + this.expiryDays * 24 * 60 * 60 * 1000,
+                format: {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                },
+            };
+        }
+        return templateInfo;
     }
 
     /**
