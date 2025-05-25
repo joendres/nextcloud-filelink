263d60aff08b477f3c88998539ed9669ecadc126
Johannes Endres <je@johannes-endres.de>
2025-05-25 17:54:53 +0200

    refactor: Rewrite the password generator
    Make sure the "one of every kind" rule is met

--- src/lib/generatePassword.js
+++ src/lib/generatePassword.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Johannes Endres
+// Copyright (C) 2021-2025 Johannes Endres
 //
 // SPDX-License-Identifier: MIT
 
@@ -9,37 +9,49 @@
 * - Enforce upper and lower case characters
 * - Enforce numeric characters
 * - Enforce special characters
-* @param {param} int length Length of password to generate
+* @param {number} length Length of password to generate
+* @returns {string} Generated password
+* @throws {Error} If length is negative
 */
 function generatePassword(length) {
-    const lower = "abcdefghijklmnopqrstuvwxyz";
-    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const numeric = "0123456789";
-    // Excludes characters that fail to output if continuous: <>
-    // Excludes characters that are hard to distinguish and easily overlooked: ,'`
-    const special = '!"#$%&()*+-./:;=?@[\\]^_{|}~';
-    const seed = lower + upper + numeric + special;
-
-    const lowerRegex = new RegExp("[" + lower + "]");
-    const upperRegex = new RegExp("[" + upper + "]");
-    const numericRegex = new RegExp("[" + numeric + "]");
-    const specialRegex = new RegExp("[" + special + "]");
-
-    let limit = 100000;
-    let i = 0;
-    let password = "";
-    while (i < limit) {
-        i++;
-        password = Array.from(Array(length)).map(() => seed[Math.floor(Math.random() * seed.length)]).join("");
-
-        if (!lowerRegex.test(password)) { continue; }
-        if (!upperRegex.test(password)) { continue; }
-        if (!numericRegex.test(password)) { continue; }
-        if (!specialRegex.test(password)) { continue; }
-
-        break;
+    if (length < 0) {
+        throw new Error("Password length must be positive");
     }
-    return password;
+
+    const pool = [
+        "abcdefghijkmnopqrstuvwxyz",
+        "ABCDEFGHJKLMNPQRSTUVWXYZ",
+        "23456789",
+        // Excludes characters that
+        // - fail to output in HTML: <>
+        // - are hard to distinguish and easily overlooked: O0lI1|'`
+        // - might confuse users because they are quoted or not: "\
+        '!#$%&()*+-./:;=?@[]^_{}~',
+    ];
+
+    let password = [];
+    if (length >= 4) {
+        // Make sure the "one of each" rule is met
+        for (const kind of pool) {
+            // Add one character of each kind to the password
+            password.push(kind[Math.floor(Math.random() * kind.length)]);
+        }
+    }
+
+    // Fill up to length with random characters
+    const joint_pool = pool.join('');
+    for (let index = password.length; index < length; index++) {
+        password.push(joint_pool[Math.floor(Math.random() * joint_pool.length)]);
+    }
+
+    // Shuffle using the Fisher-Yates algorithm
+    for (let i = password.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [password[i], password[j]] = [password[j], password[i]];
+    }
+
+    // Return the password array joint into a string
+    return password.join('');
 }
 
 /* exported generatePassword */
